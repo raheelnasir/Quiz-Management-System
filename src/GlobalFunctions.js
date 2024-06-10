b1ec5da66049c00523b35ce6870b4d1bdc0ef940
iraheeel <iraheeel@gmail.com>
2024-06-10 06:26:00 +0500

    refactor(app): finalizing quiz

--- src/GlobalFunctions.js
+++ src/GlobalFunctions.js
@@ -1,13 +1,13 @@
 import CryptoJS from 'crypto-js';
 
 export function encryptObject(obj) {
-    const key = "0027VIGO91ef46WALA21430c93c6JASON776ecbaf0229";
+    const key = process.env.HashKey;
     const ciphertext = CryptoJS.AES.encrypt(obj, key).toString();
     return ciphertext;
 }
 
 export function decryptObject(ciphertext) {
-    const key = "0027VIGO91ef46WALA21430c93c6JASON776ecbaf0229";
+    const key = process.env.HashKey;
     const bytes = CryptoJS.AES.decrypt(ciphertext, key);
     const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
     return decryptedData;
